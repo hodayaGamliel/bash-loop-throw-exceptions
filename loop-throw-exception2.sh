8244b43af39e0820338b0095be824009a97ae4ef
Hodaya Gamliel <hodaya.gamliel@takipi.com>
2015-03-23 15:08:59 +0200

    commit 2

--- loop-throw-exception2.sh
+++ loop-throw-exception2.sh
@@ -2,13 +2,15 @@
 
 PATH_TO_JAR=$1
 TIMES=$2
-
+echo " "
 echo "run $1 $2 times"
+echo " "
 
 for i in `seq 1 $TIMES`;
 do
 	echo $i
 	nohup java  -jar java  $PATH_TO_JAR &
 done
-
+echo " "
 echo "bye bye :)"
+echo " "
